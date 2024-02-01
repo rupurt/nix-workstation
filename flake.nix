f7ce1fc8e55a65280cfe9d7a14a29e84c82aa43d
Alex Kwiatkowski <alex+git@fremantle.io>
2024-02-01 13:08:07 -0800

    bump flake.lock and follow nixpkgs for all inputs

--- flake.nix
+++ flake.nix
@@ -2,20 +2,20 @@
   description = "Home Manager configuration for alex";
 
   inputs = {
-    nixpkgs-unstable.url = "github:nixos/nixpkgs/nixpkgs-unstable";
+    nixpkgs.url = "github:nixos/nixpkgs/nixpkgs-unstable";
     flake-utils.url = "github:numtide/flake-utils";
     home-manager = {
       url = "github:nix-community/home-manager";
-      inputs.nixpkgs.follows = "nixpkgs-unstable";
+      inputs.nixpkgs.follows = "nixpkgs";
     };
     nixvim = {
       url = "github:pta2002/nixvim";
-      inputs.nixpkgs.follows = "nixpkgs-unstable";
+      inputs.nixpkgs.follows = "nixpkgs";
     };
   };
 
   outputs = {
-    nixpkgs-unstable,
+    nixpkgs,
     flake-utils,
     home-manager,
     nixvim,
@@ -23,7 +23,7 @@
   }: let
     systems = ["x86_64-linux" "aarch64-linux" "x86_64-darwin" "aarch64-darwin"];
     outputs = flake-utils.lib.eachSystem systems (system: let
-      pkgs = import nixpkgs-unstable {
+      pkgs = import nixpkgs {
         inherit system;
         config.allowUnfree = true;
       };
